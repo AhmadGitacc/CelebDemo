5a5146f4bd6c9bbcfa60ac6eca390edd41ae9df4
Ahmad Dangana <danganaahmed5@gmail.com>
2025-07-26 15:29:56 +0100

    update firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
-import { getFirestore } from "firebase/firestore"; 
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBZG7KXEKahBZ4ewSIq8Uspyz2VEXlQNk",
-  authDomain: "yaza-project.firebaseapp.com",
-  projectId: "yaza-project",
-  storageBucket: "yaza-project.firebasestorage.app",
-  messagingSenderId: "282659105532",
-  appId: "1:282659105532:web:797076dab52eb6d1b8c579"
+  apiKey: "AIzaSyBYgTNyzNICXB0OGqhePr9-ZdxuC07yYNU",
+  authDomain: "celeb-demo.firebaseapp.com",
+  projectId: "celeb-demo",
+  storageBucket: "celeb-demo.firebasestorage.app",
+  messagingSenderId: "715184408711",
+  appId: "1:715184408711:web:ddcf61939a6750da285022"
 };
 
 // Initialize Firebase
